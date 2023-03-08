1f4e6494256b63029b88c8091873a887c82abc0d
nmaciasCR <nicolasmacias@hotmail.com>
2023-03-08 19:49:01 +0100

    Header Logo

--- ClientApp/src/components/Header/Header.js
+++ ClientApp/src/components/Header/Header.js
@@ -1,12 +1,17 @@
 ﻿import React from "react";
 import style from "./Header.css";
+import logoChart from "../../images/tradeAlertLogo.png";
 
 
 const Header = () => {
 
     return (
         <div className="header">
-            <h1>TradeAlert</h1>
+            <div className="logoContainer">
+                <div className="logoTittle">TradeAlert</div>
+                <img src={logoChart} height="80px" />
+            </div>
+
         </div>
     )
 
