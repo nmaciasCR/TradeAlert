34909935aa6c716dbf110311a95d3df08ee9a4cd
nmaciasCR <nicolasmacias@hotmail.com>
2023-04-13 19:21:39 +0200

    React router para la Home

--- TradeAlert/ClientApp/src/App.js
+++ TradeAlert/ClientApp/src/App.js
@@ -1,34 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Header from "./components/Header/Header";
-import QuotesGrid from "./components/QuotesGrid/QuotesGrid";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home/Home"
 
 
 const App = () => {
 
-    const [stocksList, setStockList] = useState([]);
-
-    useEffect(() => {
-        fetch("api/Stocks/GetStocks")
-            .then(response => { return response.json() })
-            .then(responseJson => {
-                setStockList(responseJson);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }, []);
-
-
     return (
-        <div>
-            <Header />
-            <div className="Container" style={{padding: "10px"}}>
-                <h2>Alta Prioridad</h2>
-                <QuotesGrid quotes={stocksList.filter(s => s.priorityId == 1)} />
-
-            </div>
-
-        </div>
+        <BrowserRouter>
+            <Route path="/">
+                <Home />
+            </Route>
+        </BrowserRouter>
     )
 
 }
---
+++ TradeAlert/ClientApp/src/pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+﻿import React, { useEffect, useState } from "react";
+import Header from "../../components/Header/Header";
+import QuotesGrid from "../../components/QuotesGrid/QuotesGrid";
+
+
+const Home = () => {
+    const [stocksList, setStockList] = useState([]);
+
+    useEffect(() => {
+        fetch("api/Stocks/GetStocks")
+            .then(response => { return response.json() })
+            .then(responseJson => {
+                setStockList(responseJson);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
+
+
+    return (
+        <div>
+            <Header />
+            <div className="Container" style={{ padding: "10px" }}>
+                <h2>Alta Prioridad</h2>
+                <QuotesGrid quotes={stocksList.filter(s => s.priorityId == 1)} />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
