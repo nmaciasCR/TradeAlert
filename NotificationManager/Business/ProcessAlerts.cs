bfb8a7b3f229ce3886f54905f30a9b304eb17ccf
nmaciasCR <nicolasmacias@hotmail.com>
2023-05-03 20:09:20 +0200

    Setear priorityId de cada accion al procesar las alertas de las mismas

--- NotificationManager/Business/ProcessAlerts.cs
+++ NotificationManager/Business/ProcessAlerts.cs
@@ -28,10 +28,16 @@ namespace NotificationManager.Business
                 //hacer un reloado de todas las colecciones de la db
                 _dbContext.ChangeTracker.Entries().ToList().ForEach(e => e.Reload());
 
+                //Setea el porcentaje de diferencia entre el valor de la alerta y la cotizacion de la accion
                 _dbContext.Quotes
                     .Include(q => q.QuotesAlerts)
                     .ToList()
                     .ForEach(q => q.QuotesAlerts.ToList().ForEach(qa => qa.regularMarketPercentDiff = GetAbsolutePercentDiff(q.regularMarketPrice, qa.price)));
+
+                //Define lr prioridad de la accion
+                _dbContext.Quotes
+                    .ToList()
+                    .ForEach(q => q.priorityId = GetStockPriority(q.QuotesAlerts.Select(qa => qa.regularMarketPercentDiff).ToList()));
 
                 _dbContext.SaveChanges();
             }
@@ -50,11 +56,14 @@ namespace NotificationManager.Business
 
         private decimal GetAbsolutePercentDiff(decimal quotePrice, decimal alertPrice)
         {
+            decimal percentDiff = 0;
             try
             {
-                decimal absDiff = Math.Abs(quotePrice - alertPrice);
-
-                decimal percentDiff = (((quotePrice + absDiff) / quotePrice) - 1) * 100;
+                if (quotePrice > 0)
+                {
+                    decimal absDiff = Math.Abs(quotePrice - alertPrice);
+                    percentDiff = (((quotePrice + absDiff) / quotePrice) - 1) * 100;
+                }
 
                 return percentDiff;
 
@@ -70,6 +79,31 @@ namespace NotificationManager.Business
             }
 
         }
+
+        /// <summary>
+        /// Retorna la prioridad de una accion
+        /// </summary>
+        private int GetStockPriority(List<decimal> priceAlertsDiff)
+        {
+            int priorityId;
+
+            if (priceAlertsDiff.Any(p => p < 5))
+            {
+                priorityId = 1;
+            }
+            else if (priceAlertsDiff.Any(p => p < 10))
+            {
+                priorityId = 2;
+            }
+            else
+            {
+                priorityId = 3;
+            }
+
+            return priorityId;
+        }
+
+
 
 
     }
